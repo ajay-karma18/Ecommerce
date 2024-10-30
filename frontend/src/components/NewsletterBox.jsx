1919ebce437208c69f784b01c7eba97f1dd77b04
ajay-karma18 <ajaykarma0018@gmail.com>
2024-10-30 20:03:10 +0530

    Update NewsletterBox.jsx

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -10,7 +10,7 @@ const NewsletterBox = () => {
     <div className=' text-center'>
       <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
       <p className='text-gray-400 mt-3'>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Join the Forever community and be the first to know about our exclusive offers, new arrivals, and styling tips! Sign up today and enjoy 20% off your next purchase.
       </p>
       <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required/>
